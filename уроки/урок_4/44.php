787c727fff2e3fa4eb1484c13fbe10dc284eb11a
may-may <vsirjdcrfz@mail.ru>
2020-11-04 20:25:08 +0400

    ответы на урок 4, добавление файлов в ветку 4

---
+++ уроки/урок_4/44.php
@@ -0,0 +1,11 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+    <title>Документы -44</title>
+</head>
+<body>
+   <h2>Я здесь поселюсь, пожалуй!</h2>
+   <br>
+   <img src="<? echo 'https://cdn.pixabay.com/photo/2015/08/28/14/54/lagoon-911963_960_720.jpg';?>" alt="!!!!!!!!!!!!!!!!!!!!!!!">
+  </body>;
+</html>

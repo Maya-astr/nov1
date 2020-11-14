b23491b31b8d45d9d43f528f559ae1dad3668a65
may-may <vsirjdcrfz@mail.ru>
2020-11-14 15:21:12 +0400

    ответы на урок-- 5, добавление файлов в ветку-- 5

---
+++ уроки/урок--5/pages/1.php
@@ -0,0 +1,28 @@
+<?php
+$nam= "Maya Mause";
+$adr_fiz= "Moskov, alleya Vitte,6 building 1, 10";
+$adr_pasp= "Gunara Astras str., bld 9, Riga, Latvia";
+$old= 79;
+$MorW= "wom";
+
+$fil1_2="D:\ярлыки\моя\программы\04.11-новый реп\nov1\рнр\pages\2.php";
+$fil1_index="D:\ярлыки\моя\программы\04.11-новый реп\nov1\рнр\pages\index.php.php";
+
+echo '<h3>  Имя старушки  ' .$nam.'</h3>' ;
+echo "Адрес прописки:   {$adr_fiz}, Адрес проживания:   {$adr_pasp} ,  Возраст срарушки , {$old}";
+?>
+<!DOCTYPE html>
+<html>
+<head><title>Рукодельницы-старушки</title>
+  </head>
+   <body>
+   <ul>
+       <li><a href= "$fil1_2"> вышивка </a></li>
+       <li><a href=  "$fil1_index"> ярмарка мастеров </a></li>
+   </ul>
+   </body>
+</html>
+
+
+
+
---
+++ уроки/урок--5/pages/index.php
@@ -0,0 +1,36 @@
+<?php
+$pag= "https://www.livemaster.ru/";
+$pag1= "https://www.livemaster.ru/item/22664283-ukrasheniya-sergi-iz-korolevskogo-yantarya-nezhnost";
+$pag2= "https://www.livemaster.ru/item/38310230-kukly-i-igrushki-tekstilnaya-kukla";
+
+$fil_ind_1="D:\ярлыки\моя\программы\04.11-новый реп\nov1\рнр\pages\1.php";
+$fil_ind_2="D:\ярлыки\моя\программы\04.11-новый реп\nov1\рнр\pages\2.php";
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title><?= $pag ?> </title>
+</head>
+<body>
+<?php
+echo '<h2> "ЯРМАРКА МАСТЕРОВ"</h2>';
+echo '<p>$pag</p>';
+echo '<p>$pag1</p>';
+echo '<p>$pag2</p>';
+?>
+<ul>
+    <li><a href=  "fil_ind_1"> </a></li>
+    <li><a href=  "fil_ind_2"> </a></li>
+</ul>
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+

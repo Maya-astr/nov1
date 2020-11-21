67064c33dce54a2eaeba8e1bdef3805ef166482b
may-may <vsirjdcrfz@mail.ru>
2020-11-21 20:40:34 +0400

    ответы на урок 4, добавление файлов в ветку 4

---
+++ урок 6/3.php
@@ -0,0 +1,21 @@
+<html >
+<head >
+    <title > решение примеров </title >
+</head >
+<body >
+<h1 > урок 6 </h1 >
+<h2> целочисленное деление
+    <?php
+    echo "<br>";
+    $chislo1= 23; $chislo2= 7;
+    echo intdiv ($chislo1,$chislo2); // вывод результата целочисленного деления
+    echo "<br>";
+    echo $chislo1 % $chislo2;        // вывод остатка от целочисленного деления
+    echo "<br>";
+
+
+    ?>
+    <a href="<?= 'index.php'?>">"возвращение в файл index"</a>
+
+
+
